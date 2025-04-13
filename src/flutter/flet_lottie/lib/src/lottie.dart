6fa05a5c632c27b82aaf703f944fd38ec808f007
ndonkoHenri <robotcoder4@protonmail.com>
2025-04-13 23:52:16 +0200

    initial commit

--- src/flutter/flet_lottie/lib/src/lottie.dart
+++ src/flutter/flet_lottie/lib/src/lottie.dart
@@ -6,17 +6,9 @@ import 'package:flutter/widgets.dart';
 import 'package:lottie/lottie.dart';
 
 class LottieControl extends StatefulWidget {
-  final Control? parent;
   final Control control;
-  final Widget? nextChild;
-  final FletControlBackend backend;
 
-  const LottieControl(
-      {super.key,
-      required this.parent,
-      required this.control,
-      required this.nextChild,
-      required this.backend});
+  const LottieControl({super.key, required this.control});
 
   @override
   State<LottieControl> createState() => _LottieControlState();
@@ -28,25 +20,31 @@ class _LottieControlState extends State<LottieControl> with FletStoreMixin {
     debugPrint(
         "Lottie build: ${widget.control.id} (${widget.control.hashCode})");
 
-    var src = widget.control.attrString("src", "")!;
-    var srcBase64 = widget.control.attrString("srcBase64", "")!;
+    var src = widget.control.getString("src", "")!;
+    var srcBase64 = widget.control.getString("srcBase64", "")!;
 
     if (src == "" && srcBase64 == "") {
       return const ErrorControl(
           "Lottie must have either \"src\" or \"src_base64\" specified.");
     }
 
-    var repeat = widget.control.attrBool("repeat");
-    var backgroundLoading = widget.control.attrBool("backgroundLoading");
-    var reverse = widget.control.attrBool("reverse");
-    var animate = widget.control.attrBool("animate");
-    var fit = parseBoxFit(widget.control.attrString("fit"));
-    FilterQuality filterQuality = parseFilterQuality(
-        widget.control.attrString("filterQuality"), FilterQuality.low)!;
-
+    var repeat = widget.control.getBool("repeat", true)!;
+    var backgroundLoading = widget.control.getBool("background_loading");
+    var reverse = widget.control.getBool("reverse", false)!;
+    var animate = widget.control.getBool("animate", true)!;
+    var fit = widget.control.getBoxFit("fit");
+    var alignment = widget.control.getAlignment("alignment");
+    var filterQuality =
+        widget.control.getFilterQuality("filter_quality", FilterQuality.low)!;
     void onWarning(String value) {
-      if (widget.control.attrBool("onError", false)!) {
-        widget.backend.triggerControlEvent(widget.control.id, "error", value);
+      if (widget.control.getBool("on_error", false)!) {
+        widget.control.triggerEvent("error", value);
+      }
+    }
+
+    void onLoaded(LottieComposition composition) {
+      if (widget.control.getBool("on_load", false)!) {
+        widget.control.triggerEvent("load");
       }
     }
 
@@ -56,14 +54,18 @@ class _LottieControlState extends State<LottieControl> with FletStoreMixin {
       if (srcBase64 != "") {
         try {
           Uint8List bytes = base64Decode(srcBase64);
-          lottie = Lottie.memory(bytes,
-              repeat: repeat,
-              reverse: reverse,
-              animate: animate,
-              fit: fit,
-              filterQuality: filterQuality,
-              backgroundLoading: backgroundLoading,
-              onWarning: onWarning);
+          lottie = Lottie.memory(
+            bytes,
+            repeat: repeat,
+            reverse: reverse,
+            animate: animate,
+            alignment: alignment,
+            fit: fit,
+            filterQuality: filterQuality,
+            backgroundLoading: backgroundLoading,
+            onLoaded: onLoaded,
+            onWarning: onWarning,
+          );
         } catch (ex) {
           return ErrorControl("Error decoding base64: ${ex.toString()}");
         }
@@ -75,9 +77,11 @@ class _LottieControlState extends State<LottieControl> with FletStoreMixin {
               repeat: repeat,
               reverse: reverse,
               animate: animate,
+              alignment: alignment,
               fit: fit,
               filterQuality: filterQuality,
               backgroundLoading: backgroundLoading,
+              onLoaded: onLoaded,
               onWarning: onWarning);
         } else {
           // URL
@@ -85,14 +89,16 @@ class _LottieControlState extends State<LottieControl> with FletStoreMixin {
               repeat: repeat,
               reverse: reverse,
               animate: animate,
+              alignment: alignment,
               fit: fit,
               filterQuality: filterQuality,
               backgroundLoading: backgroundLoading,
+              onLoaded: onLoaded,
               onWarning: onWarning);
         }
       }
 
-      return constrainedControl(context, lottie, widget.parent, widget.control);
+      return ConstrainedControl(control: widget.control, child: lottie);
     });
   }
 }
