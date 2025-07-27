47e1650ea974e961d0a1b04b23bf8d84a1e1a54b
ndonkoHenri <robotcoder4@protonmail.com>
2025-07-27 05:49:08 +0200

    initial commit

--- src/flet_lottie/lottie.py
+++ src/flet_lottie/lottie.py
@@ -1,4 +1,4 @@
-from typing import Dict, Optional
+from typing import Optional
 
 import flet as ft
 
@@ -17,36 +17,37 @@ class Lottie(ft.ConstrainedControl):
 
     src: Optional[str] = None
     """
-    The source of the Lottie file. 
-    
+    The source of the Lottie file.
+
     Can be a URL or a local [asset file](https://flet.dev/docs/cookbook/assets).
-    
+
     Note:
-        If both `src` and [`src_base64`][..] are provided, 
+        If both `src` and [`src_base64`][..] are provided,
         `src_base64` will be prioritized/used.
     """
 
     src_base64: Optional[str] = None
     """
-    The base64 encoded string of the Lottie file. 
-    
+    The base64 encoded string of the Lottie file.
+
     Note:
-        If both `src_base64` and [`src`][..] are provided, 
+        If both `src_base64` and [`src`][..] are provided,
         `src_base64` will be prioritized/used.
     """
 
     repeat: bool = True
     """
-    Whether the animation should repeat in a loop. 
-    
+    Whether the animation should repeat in a loop.
+
     Note:
         Has no effect if [`animate`][..] is `False`.
     """
 
     reverse: bool = False
     """
-    Whether the animation should be played in reverse (from start to end and then continuously from end to start). 
-    
+    Whether the animation should be played in reverse
+    (from start to end and then continuously from end to start).
+
     Note:
         Has no effect if [`animate`][..] or [`repeat`][..] is `False`.
     """
@@ -78,31 +79,33 @@ class Lottie(ft.ConstrainedControl):
 
     fit: Optional[ft.BoxFit] = None
     """
-    Defines how to inscribe the Lottie composition 
+    Defines how to inscribe the Lottie composition
     into the space allocated during layout.
     """
 
-    headers: Optional[Dict[str, str]] = None
+    headers: Optional[dict[str, str]] = None
     """
     Headers for network requests.
     """
 
     error_content: Optional[ft.Control] = None
     """
-    A control to display when an error occurs 
+    A control to display when an error occurs
     while loading the Lottie animation.
-    
+
     For more information on the error, see [`on_error`][..].
     """
 
     on_error: Optional[ft.ControlEventHandler["Lottie"]] = None
     """
     Fires when an error occurs while loading the Lottie animation.
-    
-    The `data` property of the event handler argument 
+
+    The [`data`][flet.Event.data] property of the event handler argument
     contains information on the error.
     """
 
     def before_update(self):
         super().before_update()
-        assert self.src or self.src_base64, "either src or src_base64 must be provided"
+        assert self.src or self.src_base64, (
+            "at least one of src and src_base64 must be provided"
+        )
